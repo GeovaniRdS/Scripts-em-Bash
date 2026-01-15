f4ceb079bf2afc36a1bb9a9040ea616c2e6b240b
Geovani Rodrigues <159023998+GeovaniRdS@users.noreply.github.com>
2026-01-15 19:15:15 -0300

    Add files via upload

---
+++ scriptMonitorarEspacoDisco.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 1. Define a pasta onde os relatórios devem ficar
+PASTA_DESTINO="/home/vboxuser/lab/scriptEspacoDisco/relatorios"
+
+# 2. Captura a data
+AGORA=$(date +"%d.%m.%y.%H.%M")
+NOME_ARQUIVO="Relatorio_$AGORA.txt"
+
+# 3. Define o caminho completo (Pasta + Nome)
+CAMINHO_COMPLETO="$PASTA_DESTINO/$NOME_ARQUIVO"
+
+# 4. Salva o resultado no caminho completo
+df -h > "$CAMINHO_COMPLETO"
+
+# 5. Abre exatamente o ficheiro que foi criado
+nano "$CAMINHO_COMPLETO"
